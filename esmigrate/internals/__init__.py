e5792854fbc270b3c4b1c630b6ed974821385f2b
Zobayer Hasan <zobayer1@gmail.com>
2020-06-23 23:56:07 +0600

    Update package reference

--- esmigrate/internals/__init__.py
+++ esmigrate/internals/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from esmigrate.internals.http_handler import HTTPHandler
 from esmigrate.internals.script_parser import ScriptParser
 
-__all__ = ['ScriptParser', ]
+__all__ = ['ScriptParser', 'HTTPHandler', ]
